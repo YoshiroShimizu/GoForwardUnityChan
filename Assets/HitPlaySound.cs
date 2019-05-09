8138f76c9d8509a5aee0e5a6612206643fc9e750
yoshiroX <roboxpress@yahoo.co.jp>
2019-05-09 23:27:46 +0900

    Lesson7の課題です。

---
+++ Assets/HitPlaySound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPlaySound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+    }
+    //キューブ同士、または地面に接した時に音を鳴らす
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "CubeTag" && collision.gameObject.tag == "groundtag")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
